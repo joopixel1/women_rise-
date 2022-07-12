0588f450fb6381778e7d03b9cad4113c9d7ff153
joopixel1 <joo@iastate.edu>
2022-07-12 14:07:35 -0500

    config

--- pages/models.py
+++ pages/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Stories(models.Model):
 	title = models.CharField(max_length=200)
 	body = models.TextField()
-	
+	image = models.ImageField(upload_to="images/", default='/images/default.jpg')
+
 	def __str__(self):
 		return self.title
+		
+	def get_absolute_url(self): # new
+		return reverse('post_detail', args=[str(self.id)])
--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from .views import HomePageView, StoryDetailView, StoryListView
 from django.conf import settings
+from django.conf.urls.static import static
 
 
 #make ur urls
@@ -10,4 +11,4 @@ urlpatterns = [
 	path('list/', StoryListView.as_view(), name='list'),
 	path('story/<int:pk>/', StoryDetailView.as_view(), name='detail'), # new
 	
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
